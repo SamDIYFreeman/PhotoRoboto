1b3349059de6313c104d25acc5e9ed8682f2bb72
Sam <31782351+SamDIYFreeman@users.noreply.github.com>
2024-04-08 13:36:44 -0700

    Update camera.sh

--- camera.sh
+++ camera.sh
@@ -24,7 +24,7 @@ do
 	if [ $(gpio -g read $SHUTTER) -eq 0 ]; then
 		gpio -g write $LED 0
     # takes photo and sends to printer
-		libcamera-still --width 512 --height 384 -n -o- | lp -d "EPSON-TM-T20II"
+		libcamera-still --width 512 --height 384 -n -t 1 -o- | lp
 		sleep 1
 		# Wait for user to release button before resuming
 		while [ $(gpio -g read $SHUTTER) -eq 0 ]; do continue; done
